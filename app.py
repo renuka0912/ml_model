c68489670a577c4c1ab32cf529783c4256e5d40a
renuka0912 <23215a6708@bvrit.ac.in>
2025-02-28 00:55:41 +0530

    changes

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predict():
     return render_template('index.html', prediction_text=f'Prediction: {result}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
